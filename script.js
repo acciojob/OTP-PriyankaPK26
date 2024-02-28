7cbfdaea3f19d24ea47b104fba2c5021d88e5380
PriyankaPK26 <118288268+PriyankaPK26@users.noreply.github.com>
2024-02-28 18:19:51 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-// Get all input elements with class 'code'
-const inputs = document.querySelectorAll('.code');
+// // Get all input elements with class 'code'
+// const inputs = document.querySelectorAll('.code');
 
-// Add event listeners to each input element
-inputs.forEach((input, index) => {
-  // Add input event listener to move focus to next input
-  input.addEventListener('input', (e) => {
-    // If input value is filled
-    if (input.value.length === 1) {
-      // Move focus to next input if not the last input
-      if (index < inputs.length - 1) {
-        inputs[index + 1].focus();
-      }
-    }
-  });
+// // Add event listeners to each input element
+// inputs.forEach((input, index) => {
+//   // Add input event listener to move focus to next input
+//   input.addEventListener('input', (e) => {
+//     // If input value is filled
+//     if (input.value.length === 1) {
+//       // Move focus to next input if not the last input
+//       if (index < inputs.length - 1) {
+//         inputs[index + 1].focus();
+//       }
+//     }
+//   });
 
-  // Add keydown event listener to move focus to previous input on backspace
-  input.addEventListener('keydown', (e) => {
-    // If backspace key is pressed and input value is empty and not the first input
-    if (e.key === 'Backspace' && input.value.length === 0 && index > 0) {
-      // Move focus to previous input
-      inputs[index - 1].focus();
-    }
-  });
-});
+//   // Add keydown event listener to move focus to previous input on backspace
+//   input.addEventListener('keydown', (e) => {
+//     // If backspace key is pressed and input value is empty and not the first input
+//     if (e.key === 'Backspace' && input.value.length === 0 && index > 0) {
+//       // Move focus to previous input
+//       inputs[index - 1].focus();
+//     }
+//   });
+// });
 
